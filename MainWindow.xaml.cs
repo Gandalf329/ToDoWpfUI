fa2a01f7b94af8a83f35d715e73a47ce6a412d40
pohle <pohle@ASUSN>
2022-11-06 23:13:33 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfUI1;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void SignIn_Click(object sender, RoutedEventArgs e)
+    {
+        LoginWindow loginWindow = new LoginWindow();
+        loginWindow.Show();
+        this.Visibility = Visibility.Collapsed;
+
+    }
+
+    private void Register_Click(object sender, RoutedEventArgs e)
+    {
+        RegisterWindow registerWindow = new RegisterWindow();
+        registerWindow.Show();
+        this.Visibility = Visibility.Collapsed;
+    }
+}
